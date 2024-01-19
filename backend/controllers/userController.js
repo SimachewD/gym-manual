46705a2a9b043ff70c04595e1a7efd7342eb2ad2
Simachew <simachewdenekewse@gmail.com>
2024-01-20 01:56:35 +0300

    jwt added AUTH

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,39 @@
 
-const workoutModel = require('../models/user-model');
-const mongoose = ('mongoose');
+const userModel = require('../models/user-model');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id)=>{
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'});
+}
+
 
 //login function
 const login = async (req, res)=>{
-    res.json({mssg:'Login User'});
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.login(email, password);
+        //create token
+        const token = createToken(user._id);
+        res.status(200).json(token);
+    } catch (error) {
+        res.status(400).json({ERROR: error.message});
+    }
+
 }
 
 //signup function
 const signup = async (req, res)=>{
-    res.json({mssg:'Signup User'});
+
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.signup(email, password);
+        //create token
+        const token = createToken(user._id);
+        res.status(200).json(token);
+    } catch (error) {
+        res.status(400).json({ERROR: error.message});
+    }
+
 }
 
 module.exports = {
